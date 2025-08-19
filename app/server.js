ee1a82f7178623e69d3d902939da1ff935af80b9
Rahul-Patil123 <rahulpatilccis@gmail.com>
2025-08-20 00:24:18 +0530

    Fixed some issues

--- app/server.js
+++ app/server.js
@@ -5,26 +5,27 @@ let MongoClient = require('mongodb').MongoClient;
 let bodyParser = require('body-parser');
 let app = express();
 const cors = require('cors');
-app.use(cors()); 
+app.use(cors());
 
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.get('/profile-picture', function (req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.writeHead(200, { 'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
 
 // use when starting application locally with node command
-let mongoUrlLocal = "mongodb://admin:pass@mongodb";
+let mongoUrlLocal = "mongodb://admin:pass@localhost:53758";
+
 
 // use when starting application as a separate docker container
 let mongoUrlDocker = "mongodb://admin:pass@host.docker.internal:27017";
@@ -50,19 +51,19 @@ app.post('/update-profile', function (req, res) {
     let myquery = { userid: 1 };
     let newvalues = { $set: userObj };
 
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+    db.collection("users").updateOne(myquery, newvalues, { upsert: true }, function (err, result) {
       if (err) throw err;
       client.close();
+      res.send(userObj);
     });
 
   });
   // Send response
-  res.send(userObj);
 });
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 app.get('/get-profile', function (req, res) {
   let response = {};
-  // Connect to the db
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
@@ -72,16 +73,13 @@ app.get('/get-profile', function (req, res) {
 
     db.collection("users").findOne(myquery, function (err, result) {
       if (err) throw err;
-      response = result;
       client.close();
-
-      // Send response
-      res.send(response ? response : {});
+      res.send(result || {});
     });
+
   });
 });
 
 app.listen(3000, function () {
   console.log("app listening on port 3000!");
 });
-
